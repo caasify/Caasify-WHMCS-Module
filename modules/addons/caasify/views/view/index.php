969206b7c2b41101d42420d047c2e83b84e22cc2
Salar Khosravi <salar.khosravi@gmail.com>
2024-07-31 14:51:22 +0330

    fix includes file name bug

--- modules/addons/caasify/views/view/index.php
+++ modules/addons/caasify/views/view/index.php
@@ -4,13 +4,13 @@
 <body class="container-fluid m-0 p-0" style="background-color: #ff000000 !important;">
 <div class="row py-5">
     <div class="col-12" id="app">
-        <?php include('./includes/baselayout/balancealertmodal.php');?>
+        <?php include('./includes/baselayout/balanceAlertModal.php');?>
         <div class="bg-white rounded-4 border border-2 border-body-secondary" v-cloak v-if="CommissionIsValid">
             <?php if(isset($DemoMode) && $DemoMode == 'on' ): ?>
-                <?php  include('./includes/baselayout/demoheader.php');   ?>
+                <?php  include('./includes/baselayout/demoHeader.php');   ?>
             <?php endif ?>
-            <?php include('./includes/indexparts/headtitle.php');     ?>
-            <?php include('./includes/indexparts/orderslist.php');    ?>
+            <?php include('./includes/indexparts/headTitle.php');     ?>
+            <?php include('./includes/indexparts/ordersList.php');    ?>
         </div>
         <div class="bg-white rounded-4 border border-2 border-body-secondary p-4" v-cloak v-else>
             <p class="h5 p-4 alert alert-danger">
