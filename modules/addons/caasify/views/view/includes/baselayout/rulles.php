91321506f079b1017c7465602b7d7890bc62dd8e
Salar Khosravi <salar.khosravi@gmail.com>
2024-08-11 12:08:36 +0330

    CASS-101 : fix case sensetiveness of included files

--- modules/addons/caasify/views/view/includes/baselayout/rulles.php
+++
@@ -1,3 +0,0 @@
-<?php 
-    echo('just rulles');
-?>
--- modules/addons/caasify/views/view/includes/baselayout/threespinner.php
+++
@@ -1,4 +0,0 @@
-<!-- spinner -->
-<span class="spinner-grow my-auto mb-0 ms-1 align-middle" style="--bs-spinner-width: 5px; --bs-spinner-height: 5px; --bs-spinner-animation-speed: 1s;"></span>
-<span class="spinner-grow my-auto mb-0 mx-1 align-middle" style="--bs-spinner-width: 5px; --bs-spinner-height: 5px; --bs-spinner-animation-speed: 1s;"></span>
-<span class="spinner-grow my-auto mb-0 me-1 align-middle" style="--bs-spinner-width: 5px; --bs-spinner-height: 5px; --bs-spinner-animation-speed: 1s;"></span>
