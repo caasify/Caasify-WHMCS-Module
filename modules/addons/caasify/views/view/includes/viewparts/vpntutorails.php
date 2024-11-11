368410814313856db169722a4d46c340678008c4
Salar Khosravi <salar.khosravi@gmail.com>
2024-11-11 12:07:29 +0330

    CAAS-346 : Add tutorial to view vpn page

--- modules/addons/caasify/views/view/includes/viewparts/vpntutorails.php
+++ modules/addons/caasify/views/view/includes/viewparts/vpntutorails.php
@@ -1,5 +1,5 @@
 <!-- tutorial -->
-<div class="d-flex flex-row justify-content-between align-items-center">
+<div class="d-flex flex-row justify-content-between align-items-center mb-5">
     <div class="">
         <p class="text-secondary fs-5 m-0 p-0">
             <i class="bi bi-bookmark-plus h4"></i>
@@ -9,3 +9,148 @@
         </p>
     </div>
 </div>
+<div class="accordion" id="accordionExample">
+    <!-- Windows -->
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Windows
+            </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p>
+                    <strong>App:</strong> 
+                    <a href="https://github.com/2dust/v2rayN/releases" target="_blank" class="btn btn-primary btn-sm px-4 ms-3">V2RayN</a>
+                </p>
+        
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using Subscription Link</p>
+                    <ol>
+                        <li>Open V2RayN and go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Subscription > Add</span></li>
+                        <li>Paste the subscription link and update the nodes</li>
+                        <li>Select a server and connect</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using QR Code</p>
+                    <ol>
+                        <li>In V2RayN, go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Servers > Scan QR Code</span></li>
+                        <li>Scan the QR code using your camera</li>
+                        <li>Select a server and connect</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Android -->
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Android
+            </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p>
+                    <strong>App:</strong> 
+                    <a href="https://github.com/Cenmrev/V2RayX" target="_blank" class="btn btn-primary btn-sm px-4 mx-2">V2RayX</a>
+                    <span>
+                        or 
+                    </span>
+                    <a href="https://github.com/Qv2ray/Qv2ray" target="_blank" class="btn btn-primary btn-sm px-4 mx-2">Qv2ray</a>
+                </p>
+        
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using Subscription Link</p>
+                    <ol>
+                        <li>Open V2RayN and go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Subscription</span></li>
+                        <li>Add the subscription link</li>
+                        <li>Update nodes, select a server, and connect</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using QR Code</p>
+                    <ol>
+                        <li>In V2RayN, go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Servers > Scan QR Code</span></li>
+                        <li>Scan the QR code</li>
+                        <li>Select a server and connect</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- iPhone IOS -->
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                iPhone IOS
+            </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p>
+                    <strong>App:</strong> 
+                    <a href="https://apps.apple.com/us/app/foxray/id6448898396" target="_blank" class="btn btn-primary btn-sm px-4 ms-3">Foxray</a>
+                    <a href="https://apps.apple.com/us/app/streisand/id6450534064" target="_blank" class="btn btn-primary btn-sm px-4 ms-3">Streisand</a>
+                </p>
+        
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using Subscription Link</p>
+                    <ol>
+                        <li>Open the app and go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Settings</span><span class="mx-2">or</span><span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Subscription</span></li>
+                        <li>Paste the subscription link and update the node list</li>
+                        <li>Select a server and toggle on the VPN</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using QR Code</p>
+                    <ol>
+                        <li>in the app, Go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Scan QR Code</span></li>
+                        <li>Scan the QR code</li>
+                        <li>Select a server and toggle on the VPN</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Mac OS -->
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Mac OS
+            </button>
+        </h2>
+        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <p>
+                    <strong>App:</strong> 
+                    <a href="https://github.com/2dust/v2rayNG/releases" target="_blank" class="btn btn-primary btn-sm px-4 ms-3">V2RayNG</a>
+                </p>
+        
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using Subscription Link</p>
+                    <ol>
+                        <li>Open the app and go to <span class="font-monospace bg-body-secondary p-1 px-3 mx-1">Subscription Settings</span>and paste the link</li>
+                        <li>Update the list of nodes</li>
+                        <li>Select a server and connect</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <p class="fw-medium pt-3 pb-2 mb-0">Using QR Code</p>
+                    <ol>
+                        <li>In V2RayNG, go to <span class="font-monospace bg-body-secondary p-1 px-3 ms-1">Tap the QR code icon</span></li>
+                        <li>Scan the QR code.</li>
+                        <li>Connect to a server and verify the VPN is active.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
